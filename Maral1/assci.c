6cbb3ad08009d9d70554ed042251974a3ea4f24b
Maral <maral.chahverdian@gmail.com>
2017-09-28 12:14:50 +0400

    first try

---
+++ Maral1/assci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+char c;
+printf("Enter a character:");
+
+scanf("%c,&c" );
+if(uppercase){
+c=c+32;
+
+}
+else{
+
+printf("ASCII value of %c=%d",c,c)
+return 0;
+
+
+}
+
+
+
+}

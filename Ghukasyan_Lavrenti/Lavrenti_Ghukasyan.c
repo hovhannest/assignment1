1dd18ec012a70213cf1150dacd477fdfcd7d983a
Lavrent <lavrent18@yahoo.com>
2017-10-04 16:25:59 +0400

    Add files via upload

---
+++ Ghukasyan_Lavrenti/Lavrenti_Ghukasyan.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ #include <string.h>
+ 
+ int main ()
+ {
+ 	char string[100];
+ 	printf("Write the sentence with uppercase letters:");
+  	scanf("%s", string);
+  	for(int i=0; i < strlen(string);i++)
+  	{
+  		if (string[i] >= 'A' && string[i] <= 'Z')
+  		{
+  			string[i]=string[i]+'a'-'A';
+  		}
+  	}
+ 	
+  	printf("%s\n", str);
+  	return 0;
+ }

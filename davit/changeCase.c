3a6f896b5c4c2a82c2490eb94b8bc14444d73c12
davit-abovyan <davit.abovyan@gmail.com>
2017-10-04 04:10:52 +0400

    Assignment 1

---
+++ davit/changeCase.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	char *charecter;
+	scanf("%[^\n]%*c", charecter);
+		
+	while(*charecter)
+		{
+			putchar( *charecter | 0x20); // Add 32 to shift to ASCII lowercase char
+        	charecter++;
+		}		
+		printf("%s", charecter);
+	}
+	return 0;	 
+}
